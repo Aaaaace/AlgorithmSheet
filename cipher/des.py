0d1ad12c0e15498659eccdb93df3da9bec63b43b
zheng.zhang <zheng.zhang@xgimi.com>
2021-04-04 10:34:06 +0800

    style: DES加密算法主体，但未实现分组模式及填充模式

--- cipher/des.py
+++ cipher/des.py
@@ -1,7 +1,14 @@
 from typing import Tuple
+from typing import List
 
 
 class DES:
+    '''DES加密算法
+    外部使用时调用encrypt()和decrypt()即可
+    调试时可调用_des_block_encrypt()和_des_block_decrypt()做单个数据块的加解密
+    '''
+
+    # 置换矩阵
     IP = [58, 50, 42, 34, 26, 18, 10, 2,
           60, 52, 44, 36, 28, 20, 12, 4,
           62, 54, 46, 38, 30, 22, 14, 6,
@@ -45,11 +52,9 @@ class DES:
            41, 52, 31, 37, 47, 55,
            30, 40, 51, 45, 33, 48,
            44, 49, 39, 56, 34, 53,
-           46, 42, 50, 36, 29, 32, ]
+           46, 42, 50, 36, 29, 32]
 
     KEY_OFFSET = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
-    KEY_OFFSET_ACCUMULATION = [1, 2, 4, 6, 8, 10,
-                               12, 14, 15, 17, 19, 21, 23, 25, 27, 28]
     S_BOX1 = [14, 0, 4, 15, 13, 7, 1, 4, 2, 14, 15, 2, 11, 13, 8, 1, 3, 10, 10, 6, 6, 12, 12, 11, 5, 9, 9, 5, 0, 3, 7,
               8, 4, 15, 1, 12, 14, 8, 8, 2, 13, 4, 6, 9, 2, 1, 11, 7, 15, 5, 12, 11, 9, 3, 7, 14, 3, 10, 10, 0, 5, 6, 0, 13]
     S_BOX2 = [15, 3, 1, 13, 8, 4, 14, 7, 6, 15, 11, 2, 3, 8, 4, 14, 9, 12, 7, 0, 2, 1, 13, 10, 12, 6, 0, 9, 5, 11, 10,
@@ -67,47 +72,6 @@ class DES:
     S_BOX8 = [13, 1, 2, 15, 8, 13, 4, 8, 6, 10, 15, 3, 11, 7, 1, 4, 10, 12, 9, 5, 3, 6, 14, 11, 5, 0, 0, 14, 12, 9, 7,
               2, 7, 2, 11, 1, 4, 14, 1, 7, 9, 4, 12, 10, 14, 8, 2, 13, 0, 15, 6, 12, 10, 9, 13, 0, 15, 3, 3, 5, 5, 6, 8, 11]
     S_BOX = [S_BOX1, S_BOX2, S_BOX3, S_BOX4, S_BOX5, S_BOX6, S_BOX7, S_BOX8]
-    S = [
-        [14, 4, 13,  1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9,  0,  7,
-         0, 15,  7,  4, 14,  2, 13,  1, 10,  6, 12, 11,  9,  5,  3,  8,
-         4,  1, 14,  8, 13,  6,  2, 11, 15, 12,  9,  7,  3, 10,  5,  0,
-         15, 12,  8,  2,  4,  9,  1,  7,  5, 11,  3, 14, 10,  0,  6, 13],
-
-        [15,  1,  8, 14,  6, 11,  3,  4,  9,  7,  2, 13, 12,  0,  5, 10,
-         3, 13,  4,  7, 15,  2,  8, 14, 12,  0,  1, 10,  6,  9, 11,  5,
-         0, 14,  7, 11, 10,  4, 13,  1,  5,  8, 12,  6,  9,  3,  2, 15,
-         13,  8, 10,  1,  3, 15,  4,  2, 11,  6,  7, 12,  0,  5, 14,  9],
-
-        [10,  0,  9, 14,  6,  3, 15,  5,  1, 13, 12,  7, 11,  4,  2,  8,
-         13,  7,  0,  9,  3,  4,  6, 10,  2,  8,  5, 14, 12, 11, 15,  1,
-         13,  6,  4,  9,  8, 15,  3,  0, 11,  1,  2, 12,  5, 10, 14,  7,
-         1, 10, 13,  0,  6,  9,  8,  7,  4, 15, 14,  3, 11,  5,  2, 12],
-
-        [7, 13, 14,  3,  0,  6,  9, 10,  1,  2,  8,  5, 11,  12,  4, 15,
-         13,  8, 11,  5,  6, 15,  0,  3,  4,  7,  2, 12,  1, 10, 14, 9,
-         10,  6,  9,  0, 12, 11,  7, 13, 15,  1,  3, 14,  5,  2,  8,  4,
-         3, 15,  0,  6, 10,  1, 13,  8,  9,  4,  5, 11, 12,  7,  2, 14],
-
-        [2, 12,  4,  1,  7, 10, 11,  6,  8,  5,  3, 15, 13,  0, 14,  9,
-         14, 11,  2, 12,  4,  7, 13,  1,  5,  0, 15, 10,  3,  9,  8,  6,
-         4,  2,  1, 11, 10, 13,  7,  8, 15,  9, 12,  5,  6,  3,  0, 14,
-         11,  8, 12,  7,  1, 14,  2, 13,  6, 15,  0,  9, 10,  4,  5,  3],
-
-        [12,  1, 10, 15,  9,  2,  6,  8,  0, 13,  3,  4, 14,  7,  5, 11,
-         10, 15,  4,  2,  7, 12,  9,  5,  6,  1, 13, 14,  0, 11,  3,  8,
-         9, 14, 15,  5,  2,  8, 12,  3,  7,  0,  4, 10,  1, 13, 11,  6,
-         4,  3,  2, 12,  9,  5, 15, 10, 11, 14,  1,  7,  6,  0,  8, 13],
-
-        [4, 11,  2, 14, 15,  0,  8, 13,  3, 12,  9,  7,  5, 10,  6,  1,
-         13,  0, 11,  7,  4,  9,  1, 10, 14,  3,  5, 12,  2, 15,  8,  6,
-         1,  4, 11, 13, 12,  3,  7, 14, 10, 15,  6,  8,  0,  5,  9,  2,
-         6, 11, 13,  8,  1,  4, 10,  7,  9,  5,  0, 15, 14,  2,  3, 12],
-
-        [13,  2,  8,  4,  6, 15, 11,  1, 10,  9,  3, 14,  5,  0, 12,  7,
-         1, 15, 13,  8, 10,  3,  7,  4, 12,  5,  6, 11,  0, 14,  9,  2,
-         7, 11,  4,  1,  9, 12, 14,  2,  0,  6, 10, 13, 15,  3,  5,  8,
-         2,  1, 14,  7,  4, 10,  8, 13, 15, 12,  9,  0,  3,  5,  6, 11],
-    ]
 
     P = [16, 7, 20, 21, 29, 12, 28, 17,
          1, 15, 23, 26, 5, 18, 31, 10,
@@ -115,37 +79,40 @@ class DES:
          19, 13, 30, 6, 22, 11, 4, 25]
 
     def __init__(self, key: int):
+        '''
+        Args:
+            key: 64位密钥，用十六进制整数表示
+        '''
         self.key = key
-        self.key_56bits = self.permutate(key, 64, self.PC1)
-        self.subkeys = self.key_schedule()
+        self._subkeys = self.__key_schedule()
 
-    def feistel(self, block: int, round: int):
+    def _feistel(self, block: int, round: int):
         '''费斯妥函数
 
         Args:
-            block: 数据块
+            block: 32位数据块，用十六进制整数表示
             round: 轮次，取0~15
         '''
 
-        # 扩张置换(32->48)
-        block_E = self.permutate(block, 32, self.E)
+        # 扩张置换(32位->48位)
+        block_E = self.__permutate(block, 32, DES.E)
 
         # 与密钥混合
-        block_mixed = block_E ^ self.subkeys[round]
+        block_mixed = block_E ^ self._subkeys[round]
 
-        # S盒(48->32)
+        # S盒(48位->32位)
         block_sbox = 0
         for i in range(8):
             six_bits = block_mixed >> (48 - (i+1) * 6) & 0x3f
             block_sbox <<= 4
-            block_sbox += self.S_BOX[i][six_bits]
+            block_sbox += DES.S_BOX[i][six_bits]
 
         # 置换
-        block_P = self.permutate(block_sbox, 32, self.P)
+        block_P = self.__permutate(block_sbox, 32, DES.P)
 
         return block_P
 
-    def key_schedule(self):
+    def __key_schedule(self):
         '''密钥调度，生成所有子密钥
         '''
 
@@ -160,19 +127,22 @@ class DES:
             return ((key_28bits << bits) | (key_28bits >> 28-bits)) & (0xfffffff)
 
         subkeys = []
-        key_left_part = self.key_56bits >> 28
-        key_right_part = self.key_56bits & 0xfffffff
+
+        key_56bits = self.__permutate(self.key, 64, DES.PC1)
+        key_left_part = key_56bits >> 28
+        key_right_part = key_56bits & 0xfffffff
+
         for i in range(16):
             key_left_part = rotate_left(key_left_part, self.KEY_OFFSET[i])
             key_right_part = rotate_left(key_right_part, self.KEY_OFFSET[i])
 
-
-            key_48bits = self.permutate(((key_left_part << 28) | key_right_part), 56, self.PC2)
+            key_48bits = self.__permutate(
+                ((key_left_part << 28) | key_right_part), 56, DES.PC2)
             subkeys.append(key_48bits)
-        
+
         return subkeys
 
-    def permutate(self, block: int, bits: int, permutation) -> int:
+    def __permutate(self, block: int, bits: int, permutation: List[int]) -> int:
         '''根据输入的置换矩阵对原数据进行置换
 
         Args:
@@ -187,75 +157,69 @@ class DES:
             block_result += bit
         return block_result
 
-    def _des_block_encrypt(self, plain_block):
+    def _des_block_encrypt(self, plain_block: int):
         '''单个数据块加密
 
         Args：
-            plain_block： 64位明文数据块
+            plain_block： 64位明文数据块，用十六进制整数表示
         '''
-        block = self.permutate(plain_block, 64, self.IP)
+        block = self.__permutate(plain_block, 64, DES.IP)
 
         l = block >> 32
         r = block & 0xffffffff
 
         for i in range(16):
-            l, r = r, l ^ self.feistel(r, i)
+            l, r = r, l ^ self._feistel(r, i)
 
         # 最后一轮不做交换
         l, r = r, l
         encrypted_block = l << 32 | r
 
-        return self.permutate(encrypted_block, 64, self.FP)
+        return self.__permutate(encrypted_block, 64, DES.FP)
 
-    def _des_block_decrypt(self, encrypted_block):
+    def _des_block_decrypt(self, encrypted_block: int):
         '''单个数据块解密
 
         Args:
-            encrypted_block: 64位加密数据块
+            encrypted_block: 64位加密数据块，用十六进制整数表示
         '''
-        block = self.permutate(encrypted_block, 64, self.IP)
+        block = self.__permutate(encrypted_block, 64, DES.IP)
 
         l = block >> 32
         r = block & 0xffffffff
 
         for i in range(15, -1, -1):
-            l, r = r, l ^ self.feistel(r, i)
+            l, r = r, l ^ self._feistel(r, i)
 
         # 最后一轮不做交换
         l, r = r, l
         plain_text = l << 32 | r
 
-        return self.permutate(plain_text, 64, self.FP)
+        return self.__permutate(plain_text, 64, DES.FP)
 
     def encrypt(self, plain_block: int) -> int:
+        '''加密一段明文 TODO
+        '''
         return self._des_block_encrypt(plain_block)
 
     def decrypt(self, encrypted_block: int):
+        '''解密一段密文 TODO
+        '''
         return self._des_block_decrypt(encrypted_block)
 
 
 if __name__ == '__main__':
-    d = DES(0x7CA110454A1A6E57)
-
-    plain_text = 0x01A1D6D039776742
-    print(hex(plain_text))
-
-    encypted_text = d.encrypt(plain_text)
-    print(hex(encypted_text))  # 8CA64DE9C1B123A7
-
-    decypted_text = d.decrypt(encypted_text)
-    print(hex(decypted_text))
-
-    
-    d = DES(0x0000000000000000)
-
+    # 测试数据
+    d1 = DES(0x0000000000000000)
     plain_text = 0x0000000000000000
-    print(hex(plain_text))
+    encypted_text = d1.encrypt(plain_text)
+    assert hex(encypted_text) == '0x8ca64de9c1b123a7'
+    decypted_text = d1.decrypt(encypted_text)
+    assert decypted_text == plain_text
 
-    encypted_text = d.encrypt(plain_text)
-    print(hex(encypted_text))  # 8CA64DE9C1B123A7
-
-    decypted_text = d.decrypt(encypted_text)
-    print(hex(decypted_text))
-
-    # d3 = DES(0x7CA110454A1A6E57)
+    d2 = DES(0x7CA110454A1A6E57)
+    plain_text = 0x01A1D6D039776742
+    encypted_text = d2.encrypt(plain_text)
+    assert hex(encypted_text) == '0x690f5b0d9a26939b'
+    decypted_text = d2.decrypt(encypted_text)
+    assert decypted_text == plain_text
